037af1f5ccb344fd4a56bbe19ed627edf659eaa1
Fizik05 <bboybaga13@mail.ru>
2023-10-17 13:14:18 +0300

    Added the second homework

--- My project/Assets/ForCube.cs
+++ My project/Assets/ForCube.cs
@@ -7,21 +7,64 @@ public class ForCube : MonoBehaviour
 {
     public GameObject Cylinder;
     public bool status_of_cube;
-    public Text text;
+    public float Speed = 10f;
+    private int health = 5;
+    public Text health_text;
+
+    void Start()
+    {
+        Debug.Log("Started");
+    }
+
+    void FixedUpdate()
+    {
+        if (health < 1) {
+            this.gameObject.SetActive(false);
+            health_text.text = "Game over";
+        }
+        else {
+            health_text.text = "Your health is " + health.ToString();
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * 5 * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * 5 * Speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == Cylinder) {
+            this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            health--;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("EXIT");
+        this.gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        // Debug.Log("STAY");
+    }
 
     public void onClick()
     {
-        status_of_cube = this.gameObject.activeSelf;
-        
-        if (status_of_cube)
-        {
-            this.gameObject.SetActive(false);
-            text.text = "Cube deactivated";
-        }
-        else
-        {
-            this.gameObject.SetActive(true);
-            text.text = "Cube activated";
-        }
+        health = 5;
+        this.gameObject.SetActive(true);
     }
 }
